d1965287b293985c9d73c4bbe7c388106648bb8b
Artasov <ivanhvalevskey@gmail.com>
2025-04-15 11:41:52 +0300

    process

--- entrypoint_celery.sh
+++ entrypoint_celery.sh
@@ -2,7 +2,5 @@
 echo "#####################################"
 echo "######### Celery Starting... ########"
 echo "#####################################"
-
 cd /srv/src || exit
-
 celery -A config worker -l INFO --pool=solo -E
