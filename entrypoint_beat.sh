b4c8d70abe818ba6b3283b34a8161552c2e30698
Artasov <ivanhvalevskey@gmail.com>
2025-04-15 11:47:28 +0300

    process

--- entrypoint_beat.sh
+++ entrypoint_beat.sh
@@ -3,5 +3,6 @@ echo "#####################################"
 echo "######### Beat Starting... ##########"
 echo "#####################################"
 cd /srv/src || exit
+# Если предпочитаете запускать Beat через Django-менеджер, запускаем через manage.py
 python manage.py startbeat
-#celery -A config beat -l INFO
+# celery -A config beat -l INFO
