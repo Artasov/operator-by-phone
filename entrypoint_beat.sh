6664d5b1581e87f3d6dc5ffc959bdd60f4c43b7b
Artasov <ivanhvalevskey@gmail.com>
2025-04-15 11:40:37 +0300

    process

--- entrypoint_beat.sh
+++ entrypoint_beat.sh
@@ -2,8 +2,6 @@
 echo "#####################################"
 echo "######### Beat Starting... ##########"
 echo "#####################################"
-
 cd /srv/src || exit
-
 python manage.py startbeat
 #celery -A config beat -l INFO
